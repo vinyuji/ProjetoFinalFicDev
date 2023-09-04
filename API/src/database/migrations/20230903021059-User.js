cac608173ef9cce27f882a45a13f96241670d5ad
vinyuji <vinyuji890@gmail.com>
2023-09-04 11:16:29 -0300

    migrations

--- API/src/database/migrations/20230903021059-User.js
+++ API/src/database/migrations/20230903021059-User.js
@@ -5,16 +5,27 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('users', { 
-      id: {
+    id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
     },
-      
+    Usuario: {
+      type: Sequelize.TEXT,
+      allowNull: false,
+    },
+    Cpf: {
+      type:Sequelize.INTEGER,
+      allowNull: false,
+      unique:true,
+    },
+    Cep: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
     });
   },
-
   async down (queryInterface, Sequelize) {
     await queryInterface.dropTable('users');
   }
--- API/src/database/migrations/20230903021132-Reserva.js
+++ API/src/database/migrations/20230903021132-Reserva.js
@@ -3,20 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Reserva', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      
+      });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Reserva');
   }
 };
